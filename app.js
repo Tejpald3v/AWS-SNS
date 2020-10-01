a729ae98364305c145afc7ab6a7632759eb8cf02
Tejpald3v <tejpal@d3vtech.com>
2020-10-01 16:18:44 +0530

    All files for the AWS SNS added.

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
 const express = require('express');
 const app = express();
+const LaunchDarkly = require('launchdarkly-node-server-sdk');
+const AWS = require('aws-sdk');
 require('dotenv').config();
 
-var AWS = require('aws-sdk');
 
 app.get('/', (req, res) => {
 
+    // LDClient must be a singleton
+    // Create single, shared instance of the LaunchDarkly client.
+    var ldClient = LaunchDarkly.init(process.env.YOUR_SDK_KEY);
+
     console.log("Message = " + req.query.message);
-    console.log("Number = " + req.query.number);
-    console.log("Subject = " + req.query.subject);
+
+    // Create publish parameters
     var params = {
         Message: req.query.message,
-        PhoneNumber: '+' + req.query.number,
-        MessageAttributes: {
-            'AWS.SNS.SMS.SenderID': {
-                'DataType': 'String',
-                'StringValue': req.query.subject
-            }
-        }
+        TopicArn: process.env.TOPIC
     };
 
-    var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+    // Feature accessed by specific users.
+    var user = {
+        firstName: "Omkar",
+        lastName: "Joshi",
+        key: "omkar@example.com"
+    }
 
-    publishTextPromise.then(
-        function (data) {
-            res.end(JSON.stringify({ MessageID: data.MessageId }));
-        }).catch(
-            function (err) {
-                res.end(JSON.stringify({ Error: err }));
+    // The client will emit a ready event when it has been initialized and can serve feature flags.
+    ldClient.once("ready", () => {
+
+        ldClient.variation("aws-sns", user, false,
+            (err, showFeature) => {
+                if (showFeature) {
+                    // application code to show the feature
+
+                    // Create promise and SNS service object
+                    var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+
+                    // Handle promise's fulfilled/rejected states
+                    publishTextPromise.then(
+                        function (data) {
+                            res.end(JSON.stringify({ MessageID: data.MessageId }));
+                        }).catch(
+                            function (err) {
+                                res.end(JSON.stringify({ Error: err }));
+                            });
+                } else {
+                    // the code to run if the feature is off
+                    console.log("failure");
+                    res.send("Turn the feature on!");
+                }
+                // ADDED: shut down the client, since we're about to quit
+                ldClient.close();
             });
-
+    });
 });
 
-app.listen(3000, () => console.log('SMS Service Listening on PORT 3000'))
+app.listen(3000, () => console.log('SNS Service Listening on PORT 3000'))
